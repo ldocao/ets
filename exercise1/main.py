8bab39e9daeaefe6ac1cb8057478c1287334758d
ldocao <ldocao@gmail.com>
2015-10-06 18:19:29 +0200

    content: plot prices as function of time ggplot style

--- exercise1/main.py
+++ exercise1/main.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import numpy as np
-
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.style.use('ggplot')
 
 xlfile = "../Exercise.xlsx"
 df = pd.read_excel(xlfile,sheetname="Test1", skiprows=11, header=1)
 df = df[["Unnamed: 2","Prices1", "Prices 2", "Prices 3", "Prices 4"]]
 df.columns = ["Time", "Price1", "Price2", "Price3", "Price4"]
 
+
+df.plot(x="Time")
+plt.show()
 
