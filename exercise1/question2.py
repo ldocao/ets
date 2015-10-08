80048375d16d5e2333d94e5c7c6428b7df48bcfb
ldocao <ldocao@gmail.com>
2015-10-08 16:21:13 +0200

    feat: answer question

--- exercise1/question2.py
+++ exercise1/question2.py
@@ -6,14 +6,32 @@ import matplotlib
 import ipdb
 matplotlib.style.use('ggplot')
 
+
+
+
 xlfile = "../Exercise.xlsx"
 df = pd.read_excel(xlfile,sheetname="Test1", skiprows=11, header=1)
 df = df[["Unnamed: 2","Prices1", "Prices 2", "Prices 3", "Prices 4", "Prices 5"]]
 df.columns = ["Time", "Price1", "Price2", "Price3", "Price4", "Price5"]
+df.drop(df.index[:1],inplace=True)
 
-##compute the value at risk for a period of 1 year, with risk 5%
+
+
+
+##compute the value at risk for a period of 1 year, with risk alpha in percentage
 ## https://fr.wikipedia.org/wiki/Value_at_risk
-
-df.plot(kind="hist", bins=30, alpha=0.5)
+df = df - df.mean() 
+df.plot(kind="hist", bins=40, alpha=0.5, stacked=True)
 plt.xlabel("Price")
 plt.show()
+
+alpha=0.05
+results={} #store results as dict
+for col in df.columns:
+    try:
+        results[col] = np.percentile(df[col], alpha*100)
+    except TypeError:
+        pass
+
+print results
+print "Descending risked price (alpha=0.05)", sorted(results, key=results.get)
