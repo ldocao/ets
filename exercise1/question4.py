3a35287af8564d39bf19ac3539ffa192201c0095
ldocao <ldocao@gmail.com>
2015-10-08 18:04:55 +0200

    feat: add function linear fit

--- exercise1/question4.py
+++ exercise1/question4.py
@@ -20,7 +20,18 @@ df = df.dropna()
 
 
 ##question4
-##making a prediction requires to go through feature engineering, gathering and mining data, to learn and make a prediction. However, here, with this dataset only, there is no way we can learn from the data. I could go and look for some additional data, but I have no idea which stock the prices are about, nor if there are even real. So i will go for the three most simple models I can make, ie : random walk, linear regression, and smooth curve
+##making a prediction requires to go through feature engineering, gathering and mining data, to learn and make a prediction. However, here, with this dataset only, there is no way we can learn from the data. I could go and look for some additional data, but I have no idea which stock the prices are about, nor if there are even real. So i will go for the three most simple models I can make, ie : random walk, linear regression, and smooth curve (linear on 5 points).
+
+
+
+def linear_line(x, y, n=100):
+    """Return a linear fit of y(x) with n points"""
+
+    results = np.polyfit(x, y, 1)
+    xx = np.linspace(np.min(x),np.max(x), n)
+    yy = results[1] + results[0]*xx ##linear regression
+    return xx, yy
+
 
 ## linear regression
 prices_colname = ["Price1", "Price2", "Price3", "Price4", "Price5"]
@@ -31,12 +42,19 @@ df["Time"] = x #replace time by float for plot
 
 for price in prices_colname:
     y = df[price].values
-    results = np.polyfit(x,y,1)
     print "For :", price
-    xx = np.arange(np.min(x),np.max(x))
-    yy = results[1] + results[0]*xx ##linear regression
-    #yy2 = savgol_filter(y, 11, 3)
     df.plot(kind="Scatter", x="Time", y=price)
+
+
+    ##linear regression
+    xx, yy = linear_line(x,y)
     plt.plot(xx,yy)
-    #plt.plot(xx,yy2,"g")
+
+    ##smoothing curve, linear extrapolation on last 5 points
+    xx = savgol_filter(x, 5, 1)
+    yy = savgol_filter(y, 5, 1)
+    size_x = len(x)
+    plt.plot(xx, yy, "g", linewidth=3,alpha=0.5)
+
+
     plt.show()
